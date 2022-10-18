edf37fcc577c74015d1539634dbf1f704d5e74f0
Choza-rajan <chozarajan.pandiyarajan@syncfusion.com>
2022-10-18 17:44:07 +0530

    Default project committed

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Threading;
+
+namespace UIAutomationConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
