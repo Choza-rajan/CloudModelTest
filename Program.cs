81acc1818577c4906cf9ecb1f79243104953c8e8
Choza-rajan <chozarajan.pandiyarajan@syncfusion.com>
2022-10-21 13:01:58 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -59,6 +59,28 @@ namespace UIAutomationConsole
             string switchBranchResult = switchBranchProcess.StandardOutput.ReadToEnd();
             Console.WriteLine(switchBranchResult);
 
+            var addEnvironmentMsBuildCommand = "set PATH=%PATH%;C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\BuildTools\\MSBuild\\Current\\Bin";
+            System.Diagnostics.ProcessStartInfo addEnvironmentMsBuildProcStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + addEnvironmentMsBuildCommand);
+            addEnvironmentMsBuildProcStartInfo.RedirectStandardOutput = true;
+            addEnvironmentMsBuildProcStartInfo.UseShellExecute = false;
+
+            Process addEnvironmentMsBuildProcess = new Process();
+            addEnvironmentMsBuildProcess.StartInfo = addEnvironmentMsBuildProcStartInfo;
+            addEnvironmentMsBuildProcess.Start();
+            string addEnvironmentMsBuildResult = addEnvironmentMsBuildProcess.StandardOutput.ReadToEnd();
+            Console.WriteLine(addEnvironmentMsBuildResult);
+
+            var addEnvironmentAndroidCommand = "set PATH=%PATH%;C:\\Program Files (x86)\\Android\android-sdk\\tools\\";
+            System.Diagnostics.ProcessStartInfo addEnvironmentAndroidProcStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + addEnvironmentAndroidCommand);
+            addEnvironmentAndroidProcStartInfo.RedirectStandardOutput = true;
+            addEnvironmentAndroidProcStartInfo.UseShellExecute = false;
+
+            Process addEnvironmentAndroidProcess = new Process();
+            addEnvironmentAndroidProcess.StartInfo = addEnvironmentMsBuildProcStartInfo;
+            addEnvironmentAndroidProcess.Start();
+            string addEnvironmentAndroidResult = addEnvironmentAndroidProcess.StandardOutput.ReadToEnd();
+            Console.WriteLine(addEnvironmentAndroidResult);
+
             var restoreProjectCommand = "msbuild /t:restore";
             System.Diagnostics.ProcessStartInfo restoreProjectProcStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + restoreProjectCommand);
             restoreProjectProcStartInfo.RedirectStandardOutput = true;
@@ -68,8 +90,8 @@ namespace UIAutomationConsole
             Process restoreProjectProcess = new Process();
             restoreProjectProcess.StartInfo = restoreProjectProcStartInfo;
             restoreProjectProcess.Start();
-            string restoreProjectResult3 = restoreProjectProcess.StandardOutput.ReadToEnd();
-            Console.WriteLine(restoreProjectResult3);
+            string restoreProjectResult = restoreProjectProcess.StandardOutput.ReadToEnd();
+            Console.WriteLine(restoreProjectResult);
 
             Console.WriteLine("Compilation Start");
 
@@ -101,6 +123,7 @@ namespace UIAutomationConsole
             Console.WriteLine(publishAPKResult);
 
             Console.WriteLine("Publish End");
+
         }
     }
 }
