2e2fe9cf86e5fcd88d3139eafb4ffd13ed60e74b
Choza-rajan <chozarajan.pandiyarajan@syncfusion.com>
2022-10-21 13:18:23 +0530

    Environment changes committed

--- Program.cs
+++ Program.cs
@@ -59,7 +59,7 @@ namespace UIAutomationConsole
             string switchBranchResult = switchBranchProcess.StandardOutput.ReadToEnd();
             Console.WriteLine(switchBranchResult);
 
-            var addEnvironmentMsBuildCommand = "SETX PATH=%PATH%;C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\BuildTools\\MSBuild\\Current\\Bin";
+            var addEnvironmentMsBuildCommand = "setx PATH \"%PATH%;C:\\Program Files (x86)\\Microsoft Visual Studio\\2019\\BuildTools\\MSBuild\\Current\\Bin\"";
             System.Diagnostics.ProcessStartInfo addEnvironmentMsBuildProcStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + addEnvironmentMsBuildCommand);
             addEnvironmentMsBuildProcStartInfo.RedirectStandardOutput = true;
             addEnvironmentMsBuildProcStartInfo.UseShellExecute = false;
@@ -70,7 +70,7 @@ namespace UIAutomationConsole
             string addEnvironmentMsBuildResult = addEnvironmentMsBuildProcess.StandardOutput.ReadToEnd();
             Console.WriteLine(addEnvironmentMsBuildResult);
 
-            var addEnvironmentAndroidCommand = "SETX PATH=%PATH%;C:\\Program Files (x86)\\Android\android-sdk\\tools\\";
+            var addEnvironmentAndroidCommand = "setx PATH \"%PATH%;C:\\Program Files (x86)\\Android\android-sdk\\tools\\\"";
             System.Diagnostics.ProcessStartInfo addEnvironmentAndroidProcStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + addEnvironmentAndroidCommand);
             addEnvironmentAndroidProcStartInfo.RedirectStandardOutput = true;
             addEnvironmentAndroidProcStartInfo.UseShellExecute = false;
