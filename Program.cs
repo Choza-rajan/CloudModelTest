a06ef6c24bf5b7f4ba34367d5ebb0e2b52ca9209
shivaniramakrishnan14 <97146497+shivaniramakrishnan14@users.noreply.github.com>
2022-11-01 19:07:13 +0530

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -44,7 +44,7 @@ namespace UIAutomationConsole
                 string newFolderResult = newFolderProcess.StandardOutput.ReadToEnd();
                 Console.WriteLine(newFolderResult);
 
-                var sourceClonecommand = "git clone https://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@github.com/essential-studio/sfrating-xamarin.git";
+                var sourceClonecommand = "git clone https://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@github.com/essential-studio/sfrating-xamarin.git";
                 System.Diagnostics.ProcessStartInfo sourceCloneProcStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + sourceClonecommand);
                 sourceCloneProcStartInfo.RedirectStandardOutput = true;
                 sourceCloneProcStartInfo.UseShellExecute = false;
@@ -83,7 +83,7 @@ namespace UIAutomationConsole
 
                 Console.WriteLine("Source Compilation Ended");
 
-                var testSourceClonecommand = "git clone https://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@github.com/essential-studio/sfrating-xamarin-tests.git";
+                var testSourceClonecommand = "git clone https://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@github.com/essential-studio/sfrating-xamarin-tests.git";
                 System.Diagnostics.ProcessStartInfo testSourceCloneProcStartInfo = new System.Diagnostics.ProcessStartInfo("cmd", "/c " + testSourceClonecommand);
                 testSourceCloneProcStartInfo.RedirectStandardOutput = true;
                 testSourceCloneProcStartInfo.UseShellExecute = false;
